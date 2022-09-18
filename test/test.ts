0ffb6cd9f7843bfa6b2bf00e929567fa76a66d16
Chandan1499 <gupta.chandan1499@gmail.com>
2022-09-19 02:58:54 +0530

    fix(curl formatter): added line seprators for readability and now any one can just put this curl in command propmt as well

--- test/test.ts
+++ test/test.ts
@@ -1,17 +1,15 @@
-const assert = require('assert');
-const CurlGenerator = require('../dist/curl-generator.cjs').CurlGenerator;
+const assert = require("assert");
+var CurlGenerator = require("../src/main.js").CurlGenerator;
 const data = require("../example/example-parameters");
 
 const params = data.params;
 const results = data.results;
 
-
 function test(params: Object, options: Object, result: string): void {
-	const curlSnippet = CurlGenerator(params, options)
-	assert.strictEqual(curlSnippet, result)
+  const curlSnippet = CurlGenerator(params, options);
+  assert.strictEqual(curlSnippet, result);
 }
 
-
-Object.keys(params).forEach(key => {
-	test(params[key], params[key].options, results[key]);
-})
+Object.keys(params).forEach((key) => {
+  test(params[key], params[key].options, results[key]);
+});
