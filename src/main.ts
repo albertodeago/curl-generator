56e553a0d84544628c5f824ef6473fb52f934e8f
Paul B. <paul+gh@bonaud.fr>
2024-12-11 17:10:59 +0100

    Use single quotes around URL
    This change adds single quotes around the URL of the curl command.
    
    It makes sure to make curl commands with a URL that contains multiple query param work well in *nix terminals.
    
    e.g. `curl http://example.com?query=one&query=two` the terminal will interpret the `&` character as a “send to background” command. [See this SO thread for details](https://stackoverflow.com/questions/13339469/how-to-include-an-character-in-a-bash-curl-statement)

--- src/main.ts
+++ src/main.ts
@@ -148,7 +148,7 @@ const CurlGenerator = function (
   options?: CurlAdditionalOptions
 ): string {
   let curlSnippet = "curl ";
-  curlSnippet += params.url;
+  curlSnippet += "'" + params.url + "'";
   curlSnippet += getCurlMethod(params.method);
   curlSnippet += getCurlHeaders(params.headers);
   curlSnippet += getCurlBody(params.body);
